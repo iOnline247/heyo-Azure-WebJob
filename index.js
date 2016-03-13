175dc9211d255a07ceff338fbab01e11822b00d4
Matthew Bramer <remymaza@gmail.com>
2016-03-13 01:52:19 -0500

    Added auto-maintenance of log files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
-const async = require('async');
 const restartJobInSeconds = 5 * 1000;
+const LOG_FILE_MAX_SIZE = 100; // MBs.
+const LOG_FILE_DIR_MAX_SIZE = 1; // GBs.
 const logDirectory = 'logs';
+let currentLogFileName;
+let logger;
 
 // Utils
 const Promise = require('bluebird');
@@ -12,19 +15,19 @@ const createOrUseDir = require('./utils/createOrUseDir');
 const getFolderSize = require('get-folder-size');
 const bunyan = require('bunyan');
 const randomString = require('./utils/randomString');
-const logger = bunyan.createLogger({name: 'heyo', jobId: randomString(32)});
+
 
 
 
 init();
 
 function init() {
-	// console.log('Azure-WebJob runs on Node version:', process.version);
 	createOrUseDir(logDirectory)
 		.then(getLogFiles)
+		.then(setCurrentLogFile)
 		.then(shouldTrimLogs)
 		.then(main)
-		.then(function() {
+		.finally(function() {
 			setTimeout(init, restartJobInSeconds);
 		});
 }
@@ -32,10 +35,13 @@ function init() {
 function getLogFiles(dirPath) {
 	return new Promise(function(resolve, reject) {
 		const files = fs.readdirSync(dirPath).map(function(v) {
+			const file = fs.statSync(path.join(dirPath, v));
+
 			return { 
 				name: v,
-				time: fs.statSync(path.join(dirPath, v)).mtime.getTime()
-			}; 
+				size: Math.floor(file.size / 1024 / 1024), // converts bytes to MB.
+				time: file.mtime.getTime()
+			};
 		});
 
 		resolve({
@@ -44,19 +50,49 @@ function getLogFiles(dirPath) {
 		});
 	});
 }
-// setTimeout(init);
+
+function setCurrentLogFile(deets) {
+	// set currenLogFileName based on size of last modified log file.
+	// 100 MBs is the limit per log file.
+
+	return new Promise(function(resolve, reject) {
+		const lastModifiedLogFile = deets.files.sort(function(a, b) {
+			return b.time - a.time;
+		})[0];
+
+		// if less than 100 MBs, use it, else create new logFile and set currentLogFileName.
+		if (lastModifiedLogFile.size >= LOG_FILE_MAX_SIZE) {
+			// create new log file.
+			const logFileName = 'log - ' + (new Date()).getTime() + '.log';
+			currentLogFileName = path.join(logDirectory, logFileName);
+		} else {
+			currentLogFileName = path.join(logDirectory, lastModifiedLogFile.name);
+		}
+		
+		logger = bunyan.createLogger({
+			name: 'heyo',
+			jobId: randomString(32),
+			streams: [{
+				path: currentLogFileName
+			}]
+		});
+		resolve(deets);
+	});
+}
+
 function shouldTrimLogs(deets) {
 	return new Promise(function(resolve, reject) {
 		getFolderSize(deets.dirPath, function(err, dirSize) {
-			// dirSize is in GBs now.
+			// dirSize is in GBs.
 			dirSize = (dirSize / 1024 / 1024 / 1024);
 			logger.info('Log directory size: ' + dirSize + ' GBs.');
-
-			if(dirSize > 1) {
+			
+			// Greater than 1 GB.
+			if(dirSize > LOG_FILE_DIR_MAX_SIZE) {
 				deletingOldestLogFile(deets.files);
 			}
 
-			resolve();		
+			resolve(deets);		
 		});
 	});
 }
@@ -66,13 +102,12 @@ function deletingOldestLogFile(files) {
 		return a.time - b.time;
 	})[0];
 
-	debugger;
-
-	// implement deletion of file.
+	fs.unlinkSync(path.join(logDirectory, oldLogFile.name));
 }
 
 function main() {
 	return new Promise(function(resolve, reject) {
+		// fs.appendFileSync(currentLogFileName, logger.info('main() invoked'));
 		logger.info('main() invoked');
 		resolve();
 	});
