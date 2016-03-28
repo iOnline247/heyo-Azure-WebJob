644c5c057d8b6b88340f07949e8d94e29eb3ddaf
Matthew Bramer <remymaza@gmail.com>
2016-03-28 14:26:45 -0400

    Reworked rotating log files

--- index.js
+++ index.js
@@ -1,114 +1,87 @@
 'use strict';
-const fs = require('fs');
+const settings = require('./config/secrets');
 const path = require('path');
-const restartJobInSeconds = 5 * 1000;
-const LOG_FILE_MAX_SIZE = 100; // MBs.
-const LOG_FILE_DIR_MAX_SIZE = 1; // GBs.
-const logDirectory = 'logs';
-let currentLogFileName;
-let logger;
+const restartJobInSeconds = 100;
+const LOG_FILE_MAX_SIZE = 100 * 1024 * 1024; // MBs.
+const MAX_NUM_OF_LOG_FILES = 10; // 10 * 100 MBs = 1 GB.
+const logDirectory = (process.env.WEBROOT_PATH) ?
+	path.join(process.env.WEBROOT_PATH, 'logs') :
+	'logs';
+
+// DB
+const db = require('monk')(settings.mongo.conn);
+const users = db.get('users');
+/*
+// Tenative user schema
+{
+	access_token: string,
+	access_token_secret: string,
+	request_token: string,
+	user_id: string,
+	phone_number: string, // xxx-xxx-xxxx
+	memes: Array
+	messages: Array
+}
+*/
 
 // Utils
 const Promise = require('bluebird');
 const _ = require('lodash');
-const createOrUseDir = require('./utils/createOrUseDir');
-const getFolderSize = require('get-folder-size');
-const bunyan = require('bunyan');
-const randomString = require('./utils/randomString');
+const winston = require('winston');
+const logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.File)({
+			name: 'default',
+			filename: path.join(logDirectory, 'web-job.log'),
+			level: 'info',
+			logstash: true,
+			maxsize: LOG_FILE_MAX_SIZE,
+			maxFiles: MAX_NUM_OF_LOG_FILES,
+			tailable: true
+		}),
+		new (winston.transports.File)({
+			name: 'error-file',
+			filename: path.join(logDirectory, 'web-job.log'),
+			level: 'error',
+			logstash: true,
+			maxsize: LOG_FILE_MAX_SIZE,
+			maxFiles: MAX_NUM_OF_LOG_FILES,
+			tailable: true
+		})
+	]
+});
+const cuid = require('cuid');
 
-
-
-
-init();
 
 function init() {
-	createOrUseDir(logDirectory)
-		.then(getLogFiles)
-		.then(setCurrentLogFile)
-		.then(shouldTrimLogs)
-		.then(main)
+	main()
+		.catch(function() {
+			// TODO:
+			// Handle error within main.
+			debugger;
+		})
 		.finally(function() {
 			setTimeout(init, restartJobInSeconds);
 		});
 }
 
-function getLogFiles(dirPath) {
-	return new Promise(function(resolve, reject) {
-		const files = fs.readdirSync(dirPath).map(function(v) {
-			const file = fs.statSync(path.join(dirPath, v));
-
-			return { 
-				name: v,
-				size: Math.floor(file.size / 1024 / 1024), // converts bytes to MB.
-				time: file.mtime.getTime()
-			};
-		});
-
-		resolve({
-			dirPath: dirPath, 
-			files: files
-		});
-	});
-}
-
-function setCurrentLogFile(deets) {
-	// set currenLogFileName based on size of last modified log file.
-	// 100 MBs is the limit per log file.
-
-	return new Promise(function(resolve, reject) {
-		const lastModifiedLogFile = deets.files.sort(function(a, b) {
-			return b.time - a.time;
-		})[0];
-
-		// if less than 100 MBs, use it, else create new logFile and set currentLogFileName.
-		if (lastModifiedLogFile.size >= LOG_FILE_MAX_SIZE) {
-			// create new log file.
-			const logFileName = 'log - ' + (new Date()).getTime() + '.log';
-			currentLogFileName = path.join(logDirectory, logFileName);
-		} else {
-			currentLogFileName = path.join(logDirectory, lastModifiedLogFile.name);
-		}
-		
-		logger = bunyan.createLogger({
-			name: 'heyo',
-			jobId: randomString(32),
-			streams: [{
-				path: currentLogFileName
-			}]
-		});
-		resolve(deets);
-	});
-}
-
-function shouldTrimLogs(deets) {
-	return new Promise(function(resolve, reject) {
-		getFolderSize(deets.dirPath, function(err, dirSize) {
-			// dirSize is in GBs.
-			dirSize = (dirSize / 1024 / 1024 / 1024);
-			logger.info('Log directory size: ' + dirSize + ' GBs.');
-			
-			// Greater than 1 GB.
-			if(dirSize > LOG_FILE_DIR_MAX_SIZE) {
-				deletingOldestLogFile(deets.files);
-			}
-
-			resolve(deets);		
-		});
-	});
-}
-
-function deletingOldestLogFile(files) {
-	const oldLogFile = files.sort(function(a, b) {
-		return a.time - b.time;
-	})[0];
-
-	fs.unlinkSync(path.join(logDirectory, oldLogFile.name));
-}
-
 function main() {
+	const logId = cuid();
 	return new Promise(function(resolve, reject) {
-		// fs.appendFileSync(currentLogFileName, logger.info('main() invoked'));
-		logger.info('main() invoked');
+		logger.info('Hello world!', {logId: logId});
+		logger.info('main() invoked', {logId: logId});
 		resolve();
 	});
 }
+
+init();
+
+
+
+// Catch unhandled errors
+process.on('uncaughtException', function (err) {
+	let message = '\n' + (new Date).toUTCString() + ' uncaughtException: ' + err.message;
+	
+	message += '\n\n' + err.stack;
+	logger.error(message, {logId: cuid()});
+});
